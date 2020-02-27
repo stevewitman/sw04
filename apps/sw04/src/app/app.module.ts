cc65dd86747fd44248861147e6f920fa600fe1f5
Steve Witman <stevewitman@gmail.com>
2020-02-27 09:07:05 -0700

    Added ui to projects-list and project-detail components

--- apps/sw04/src/app/app.module.ts
+++ apps/sw04/src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { MaterialModule } from '@bb/material';
 import { CoreDataModule } from '@bb/core-data';
 import { UiToolbarModule } from '@bb/ui-toolbar';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ProjectsComponent } from './projects/projects.component';
 import { ProjectsListComponent } from './projects/projects-list/projects-list.component';
 import { ProjectDetailsComponent } from './projects/project-details/project-details.component';
@@ -22,7 +23,9 @@ import { ProjectDetailsComponent } from './projects/project-details/project-deta
     MaterialModule,
     CoreDataModule,
     UiToolbarModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
